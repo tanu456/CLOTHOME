970cd95f62c40c07e519a853f77c1574214ee323
Tanya <tanya1998agarwal43@gmail.com>
2021-05-07 12:42:18 +0530

    database linked

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 800;
+const mongoose = require('mongoose');
+const bodyparser = require("body-parser");
+var urlencodedparser = bodyparser.urlencoded({extended: false});
+
+mongoose.connect('mongodb://localhost/database', {useNewUrlParser: true, useUnifiedTopology: true});
+
+//define schema
+const contactSchema = new mongoose.Schema({
+    name: String,
+    phone: Number,
+    email: String,
+    address: String,
+
+  });
+
+  const Contact = mongoose.model('contact', contactSchema);
 // EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static')) // For serving static files
 
@@ -14,11 +30,24 @@ app.get('/', (req, res)=>{
     const params = {}
     res.status(200).render('home.pug', params);
 })
-app.get('/Contact',(req,res)=>{
+app.get('/contact',(req,res)=>{
+    console.log("Y IS")
    const params = { }
    res.status(200).render('Contact.pug',params) 
 })
+
+app.post('/contact',urlencodedparser, (hp,res) =>{
+    console.log(hp.body);
+    var myData = new Contact(hp.body);
+    myData.save().then(()=>{
+    res.send("This item has been saved to the database")
+    }).catch(()=>{
+    res.status(400).send("item was not saved to the databse")
+})
+})
+
+
 // START THE SERVER
 app.listen(port, ()=>{
     console.log(`The application started successfully on port ${port}`);
-});
+})
